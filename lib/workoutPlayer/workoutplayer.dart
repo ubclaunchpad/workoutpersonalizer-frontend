f52a4cedc63e89ba60ad0ba5f902be1c6d7037b6
Andi Xiong <xiongandi@gmail.com>
2021-11-06 13:53:06 -0700

    init state and switch to ListView

--- lib/workoutPlayer/workoutplayer.dart
+++ lib/workoutPlayer/workoutplayer.dart
@@ -2,16 +2,9 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:video_player/video_player.dart';
 import 'package:circular_countdown_timer/circular_countdown_timer.dart';
+import 'package:workoutpersonalizer_frontend/models/Exercise.dart';
 
 import 'chewie_list_item.dart';
-
-// void workoutplayer() {
-//   runApp(
-//     MaterialApp(
-//       home: WorkoutPlayer(),
-//     ),
-//   );
-// }
 
 /// This is the stateful widget that the main application instantiates.
 class WorkoutPlayer extends StatefulWidget {
@@ -24,6 +17,25 @@ class WorkoutPlayer extends StatefulWidget {
 /// This is the private State class that goes with MyStatefulWidget.
 class _WorkoutPlayer extends State<WorkoutPlayer> {
   final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+  List<Exercise> exercises = [];
+
+  @override
+  void initState() {
+    super.initState();
+    // TODO: fetch exercises here
+    String dummyThumbnailUrl = "https://www.inposture.com/wp-content/uploads/2020/05/Sit-ups.jpg";
+    String dummyVideoUrl = "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4";
+    exercises = [
+      Exercise(1, "Exercise 1", "Exercise1 Description", dummyThumbnailUrl, dummyVideoUrl, 1),
+      Exercise(2, "Exercise 2", "Exercise2 Description", dummyThumbnailUrl, dummyVideoUrl, 1),
+      Exercise(3, "Exercise 3", "Exercise3 Description", dummyThumbnailUrl, dummyVideoUrl, 10),
+      Exercise(4, "Exercise 4", "Exercise4 Description", dummyThumbnailUrl, dummyVideoUrl, 60),
+      Exercise(5, "Exercise 5", "Exercise5 Description", dummyThumbnailUrl, dummyVideoUrl, 90),
+      Exercise(6, "Exercise 6", "Exercise6 Description", dummyThumbnailUrl, dummyVideoUrl, 120),
+      Exercise(7, "Exercise 7", "Exercise7 Description", dummyThumbnailUrl, dummyVideoUrl, 191),
+      Exercise(8, "Exercise 8", "Exercise8 Description", dummyThumbnailUrl, dummyVideoUrl, 1000),
+    ];
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -33,6 +45,7 @@ class _WorkoutPlayer extends State<WorkoutPlayer> {
       body: Row(
         children: [
           Expanded(
+            flex: 3,
             child: Column(
               children: [
                 videoPlayerFunction(context),
@@ -40,9 +53,19 @@ class _WorkoutPlayer extends State<WorkoutPlayer> {
               ]
             ),
           ),
-          Positioned(
-            right: 0,
-            child: workoutList(context)
+          Expanded(
+            flex: 1,
+            child: Container(
+              color: Colors.grey[300],
+              child: Column(
+                children: [
+                  workoutTitle(),
+                  Expanded(
+                    child: workoutList(context, exercises),
+                  )
+                ],
+              )
+            )
           )
         ]
       ),
@@ -59,7 +82,7 @@ theAppBar() {
             Text('WorkoutApp'), 
             OutlinedButton(onPressed: () {}, child: const Text('DashBoard', style: TextStyle(color: Colors.white))),
             OutlinedButton(onPressed: () {}, child: const Text('My Workouts', style: TextStyle(color: Colors.white))),
-            ]
+          ]
         ),
         actions: [
           ClipOval(
@@ -88,7 +111,7 @@ Widget videoPlayerFunction(BuildContext context)  {
         width: MediaQuery.of(context).size.width,
         height: MediaQuery.of(context).size.height * 0.75,
         child: AspectRatio(
-          aspectRatio: 16/9, // should be aspect ratio of the video
+          aspectRatio: 16/9, // TODO: should be the aspect ratio of the video
           child: ChewieListItem(
             videoPlayerController: VideoPlayerController.network('http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4')
           )
@@ -130,68 +153,65 @@ Widget videoDescription() {
 }
 
 Widget workoutTitle() { 
-  return Text('This is the Workout:', textAlign: TextAlign.left, style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white, fontSize: 30) );
-}
-
-Widget workoutList(BuildContext context) { 
-  return Container(
-    decoration: BoxDecoration(color: Colors.blueGrey),
-    child: SingleChildScrollView(
-      child: Row(
-        children: [
-          Column(
-            children: [
-              workoutTitle(),
-              SizedBox(height: 10),
-              workout(context, 'Workout 1'),
-              SizedBox(height: 10),
-              workout(context, 'Workout 2'),
-              SizedBox(height: 10),
-              workout(context, 'Workout 3'),
-              SizedBox(height: 10),
-              workout(context, 'Workout 4'),
-              SizedBox(height: 10),
-              workout(context, 'Workout 5'),
-              SizedBox(height: 10),
-              workout(context, 'Workout 6'),             
-              SizedBox(height: 10),
-              workout(context, 'Workout 7'),
-            ]
-          )
-        ],
-      )
-    )
+  return Text(
+    'Workout Title', 
+    textAlign: TextAlign.left, 
+    style: TextStyle(
+      fontWeight: FontWeight.bold, 
+      color: Colors.black, 
+      fontSize: 30
+    ) 
   );
 }
 
+Widget workoutList(BuildContext context, List<Exercise> exercises) { 
+  return exercises.isNotEmpty
+    ? ListView.builder(
+        itemCount: exercises.length,
+        itemBuilder: (BuildContext context, int index) {
+          return workout(context, exercises[index]);
+        }
+      )
+      : Row(
+        children: const [
+          CircularProgressIndicator(),
+          Text("Loading")
+        ] 
+      );
+}
 
-Widget workout(BuildContext context, String numberOfWorkOut) { 
+
+Widget workout(BuildContext context, Exercise exercise) { 
+  Duration duration = Duration(seconds: exercise.length);
+  String sDuration = "${duration.inMinutes.toString().padLeft(2, "0")}:${duration.inSeconds.remainder(60).toString().padLeft(2, "0")}";
   return Row(
+    // alignment: WrapAlignment.spaceAround,
     children: [
-      Column(
-        children: [
-          Container( 
-          height: 100, 
-          width: MediaQuery.of(context).size.width / 6,
-          decoration: BoxDecoration(color: Colors.grey),
-        )]
+      Container( 
+        margin: EdgeInsets.all(8.0),
+        height: 100, 
+        width: MediaQuery.of(context).size.width / 6,
+        decoration: BoxDecoration(color: Colors.grey),
       ),
       Column(
-        // ignore: prefer_const_literals_to_create_immutables
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
           Text(
-            numberOfWorkOut, 
+            exercise.name, 
             style: TextStyle(
               fontWeight: FontWeight.bold, 
-              color: Colors.white, 
-              fontSize: 15)
-            ),
+              color: Colors.black, 
+              fontSize: 15
+            )
+          ),
           Text(
-            '0:00-2:00', 
+            sDuration,
             style: TextStyle(
               fontWeight: FontWeight.bold, 
-              color: Colors.white, 
-              fontSize: 10)
+              color: Colors.black, 
+              fontSize: 10
+            )
           ),
         ],
       )
