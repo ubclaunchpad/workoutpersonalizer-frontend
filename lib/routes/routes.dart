8e8ab4f616aa5a4c57de2814e3211a6d440d51bc
Lance Tan <ltan0521@gmail.com>
2021-11-18 23:17:28 -0800

    Removed redundant files

--- lib/routes/routes.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/foundation.dart';
-import 'package:flutter/material.dart';
-
-const String exploreRoute = '/explore';
-const String myWorkoutsRoute = '/myworkouts';
-const String creatorsCornerRoute = '/creatorscorner';
-
-final navKey = GlobalKey<NavigatorState>();
