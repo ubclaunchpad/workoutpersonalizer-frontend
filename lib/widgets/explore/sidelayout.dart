610f660da4470f9c8e3186dab3126f95e4a6d592
Xavier Lam <reivax685@gmail.com>
2022-02-11 23:58:57 -0800

    added remaining filters to sidebar

--- lib/widgets/explore/sidelayout.dart
+++ lib/widgets/explore/sidelayout.dart
@@ -7,7 +7,7 @@ class SideLayout extends StatefulWidget {
 }
 
 class SideLayoutState extends State<SideLayout> {
-  Map<String, bool> values = {
+  Map<String, bool> muscleGroupValues = {
     'Chest': false,
     'Back': false,
     'Arms': false,
@@ -18,11 +18,31 @@ class SideLayoutState extends State<SideLayout> {
     'Neck': false,
   };
 
+  Map<String, bool> exerciseTypeValues = {
+    'Cardio': false,
+    'Endurance': false,
+    'HIIT': false,
+    'Pilates': false,
+    'Weight training': false,
+    'Yoga': false,
+  };
+
+  Map<String, bool> difficultyLevelValues = {
+    'Easy': false,
+    'Intermediate': false,
+    'Advanced': false,
+  };
+
+  Map<String, bool> equiptmentValues = {
+    'Weight': false,
+    'Resistance bands': false,
+  };
+
   @override
   Widget build(BuildContext context) {
     return Expanded(
       child: Container(
-          padding: EdgeInsets.symmetric(vertical: 20, horizontal: 20),
+          padding: EdgeInsets.symmetric(vertical: 30, horizontal: 30),
           color: Color(0x00000000),
           child: Column(
             children: [
@@ -42,24 +62,126 @@ class SideLayoutState extends State<SideLayout> {
               Expanded(
                 child: ListView(
                   controller: ScrollController(),
-                  children: values.keys.map((String key) {
+                  children: muscleGroupValues.keys.map((String key) {
                     return CheckboxListTile(
                       contentPadding: const EdgeInsets.all(0),
                       dense: true,
                       controlAffinity: ListTileControlAffinity.leading,
                       title: Text(key, style: filterItemStyle),
-                      value: values[key],
+                      value: muscleGroupValues[key],
                       onChanged: (bool? value) {
                         if (value != null) {
                           setState(() {
-                            values[key] = value;
+                            muscleGroupValues[key] = value;
                           });
                         }
                       },
                     );
                   }).toList(),
                 ),
-              )
+              ),
+              Row(
+                children: const [
+                  CircleAvatar(
+                    backgroundColor: Color(0xFFFFCD78),
+                    child: Icon(Icons.self_improvement,
+                        size: 30, color: Colors.black),
+                  ),
+                  Text(
+                    " Exercise Types",
+                    style: filterHeaderStyle,
+                  ),
+                ],
+              ),
+              Expanded(
+                child: ListView(
+                  controller: ScrollController(),
+                  children: exerciseTypeValues.keys.map((String key) {
+                    return CheckboxListTile(
+                      contentPadding: const EdgeInsets.all(0),
+                      dense: true,
+                      controlAffinity: ListTileControlAffinity.leading,
+                      title: Text(key, style: filterItemStyle),
+                      value: exerciseTypeValues[key],
+                      onChanged: (bool? value) {
+                        if (value != null) {
+                          setState(() {
+                            exerciseTypeValues[key] = value;
+                          });
+                        }
+                      },
+                    );
+                  }).toList(),
+                ),
+              ),
+              Row(
+                children: const [
+                  CircleAvatar(
+                    backgroundColor: Color(0xFFFFCD78),
+                    child: Icon(Icons.trending_up_sharp,
+                        size: 30, color: Colors.black),
+                  ),
+                  Text(
+                    " Difficulty Level",
+                    style: filterHeaderStyle,
+                  ),
+                ],
+              ),
+              Expanded(
+                child: ListView(
+                  controller: ScrollController(),
+                  children: difficultyLevelValues.keys.map((String key) {
+                    return CheckboxListTile(
+                      contentPadding: const EdgeInsets.all(0),
+                      dense: true,
+                      controlAffinity: ListTileControlAffinity.leading,
+                      title: Text(key, style: filterItemStyle),
+                      value: difficultyLevelValues[key],
+                      onChanged: (bool? value) {
+                        if (value != null) {
+                          setState(() {
+                            difficultyLevelValues[key] = value;
+                          });
+                        }
+                      },
+                    );
+                  }).toList(),
+                ),
+              ),
+              Row(
+                children: const [
+                  CircleAvatar(
+                    backgroundColor: Color(0xFFFFCD78),
+                    child: Icon(Icons.fitness_center,
+                        size: 30, color: Colors.black),
+                  ),
+                  Text(
+                    " Equiptment",
+                    style: filterHeaderStyle,
+                  ),
+                ],
+              ),
+              Expanded(
+                child: ListView(
+                  controller: ScrollController(),
+                  children: equiptmentValues.keys.map((String key) {
+                    return CheckboxListTile(
+                      contentPadding: const EdgeInsets.all(0),
+                      dense: true,
+                      controlAffinity: ListTileControlAffinity.leading,
+                      title: Text(key, style: filterItemStyle),
+                      value: equiptmentValues[key],
+                      onChanged: (bool? value) {
+                        if (value != null) {
+                          setState(() {
+                            equiptmentValues[key] = value;
+                          });
+                        }
+                      },
+                    );
+                  }).toList(),
+                ),
+              ),
             ],
           )),
     );
