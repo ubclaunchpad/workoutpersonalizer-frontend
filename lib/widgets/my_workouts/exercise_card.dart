06f9242a7d1045af5fbb6e53f4b5d11035a6f885
daman917 <daman.sohi@ymail.com>
2022-02-19 01:56:20 -0800

    Added Exercise cards to My Workouts page

---
+++ lib/widgets/my_workouts/exercise_card.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:workoutpersonalizer_frontend/constants/styles.dart';
+import 'package:workoutpersonalizer_frontend/models/exercise.dart';
+
+class ExerciseCard extends StatelessWidget {
+  final Exercise exercise;
+  const ExerciseCard({Key? key, required this.exercise}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
